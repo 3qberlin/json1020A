29756dfc110d4a6724d698d397e0d39a2ee1a4bc
Berlin <3q.berlin@gmail.com>
2023-10-23 22:39:10 +0800

    更新 render網址 取代localhost

--- page.js
+++ page.js
@@ -1,6 +1,7 @@
 // http://127.0.0.1:5500/page.html?id=3
 const id = location.href.split("=")[1];
 const _url = "http://localhost:3000"; // 修改處
+var _targetUrl = "https://json1020a.onrender.com"; // 修改處
 axios.get(`${_url}/todos/${id}`)
     .then(function(response){
         document.querySelector("h1").textContent = response.data.id
